4b14691d9f0068e63730dbc1a6ebe4ac9c528900
Webdesigntomakethebest <130653495+Webdesigntomakethebest@users.noreply.github.com>
2023-04-13 11:44:00 +0200

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,38 @@
+ let open = document.querySelector('#open');
+ let close = document.querySelector('#close-btn');
+ let container = document.querySelector('#container');
+ // Add EventListener 
+ open.addEventListener('click', function(){
+    container.style.display = 'block';
+ });
+ close.addEventListener('click', function(){
+    container.style.display = 'none';
+ });
+ // P1057991513
+/*
+Client ID: AQcoWh4nj29Yr0s-53CjaLOf4pPmtyxfy820_YqC
+RM353md9njXLwVt1rRGf0-mBqf3lwIRRiuf5IEdS
+
+Client Secret: 
+EE2ZhWew1I4_ToWLPjqOugBe2CiOA5jK7BCMKxmu
+PtrIFoi2Zxz8z4lHHNo3tOLmXqa1XgbrONooSKWu
+
+
+
+
+
+Project ID: symmetric-flag-383607. It cannot be changed later  sign in with gougle
+
+* */
+// My own stff 
+/*
+Client ID : 
+AQcoWh4nj29Yr0s-53CjaLOf4pPmtyxfy820_YqC
+RM353md9njXLwVt1rRGf0-mBqf3lwIRRiuf5IEdS
+
+client Secret : 
+EE2ZhWew1I4_ToWLPjqOugBe2CiOA5jK7BCMKxmu
+PtrIFoi2Zxz8z4lHHNo3tOLmXqa1XgbrONooSKWu
+
+
+*/
